169a87b3b174415b72592a244f8104959a9a78f2
Prabin Gautam <prabeen122@gmail.com>
2026-04-22 11:17:06 +0545

    fix url issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: env('DATABASE_URL'),
-    directUrl: env('DIRECT_URL'),  
+    url: env('DIRECT_URL'),
+    // directUrl: env('DIRECT_URL'),  
   },
 });
